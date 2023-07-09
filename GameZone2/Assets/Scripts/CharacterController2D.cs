b05bebc1149c8d3df39fded0ba9800cb5b3e6216
senayzz <200706021@st.maltepe.edu.tr>
2023-07-09 17:24:00 +0300

    videodaki kodları denedik 1. part

---
+++ GameZone2/Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,146 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterController2D : MonoBehaviour
+{
+	[SerializeField] private float jumpSpeed = 400f;							// Amount of force added when the player jumps.
+	// [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
+	[Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f;	// How much to smooth out the movement
+	[SerializeField] private bool airControl = false;							// Whether or not a player can steer while jumping;
+	[SerializeField] private LayerMask whatIsGround;							// A mask determining what is ground to the character
+	[SerializeField] private Transform groundCheck;							// A position marking where to check if the player is grounded.
+	[SerializeField] private Transform ceilingCheck;							// A position marking where to check for ceilings
+	[SerializeField] private Collider2D crouchDisableCollider;				// A collider that will be disabled when crouching
+
+	const float groundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+	private bool grounded;            // Whether or not the player is grounded.
+	const float ceilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
+	private Rigidbody2D rb;
+	private bool facingRight = true;  // For determining which way the player is currently facing.
+	private Vector3 velocity = Vector3.zero;
+
+	[Header("Events")]
+	[Space]
+
+	public UnityEvent OnLandEvent;
+
+	[System.Serializable]
+	public class BoolEvent : UnityEvent<bool> { }
+
+	// public BoolEvent OnCrouchEvent;
+	// private bool m_wasCrouching = false;
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+
+		if (OnLandEvent == null)
+			OnLandEvent = new UnityEvent();
+
+		// if (OnCrouchEvent == null)
+		// 	OnCrouchEvent = new BoolEvent();
+	}
+
+	private void FixedUpdate()
+	{
+		bool wasGrounded = grounded;
+		grounded = false;
+
+		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+			{
+				grounded = true;
+				if (!wasGrounded)
+					OnLandEvent.Invoke();
+			}
+		}
+	}
+
+
+	public void Move(float move, bool jump)
+	{
+		// If crouching, check to see if the character can stand up
+		// if (!crouch)
+		// {
+		// 	// If the character has a ceiling preventing them from standing up, keep them crouching
+		// 	if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
+		// 	{
+		// 		crouch = true;
+		// 	}
+		// }
+
+		//only control the player if grounded or airControl is turned on
+		if (grounded || airControl)
+		{
+
+			// If crouching
+			// if (crouch)
+			// {
+			// 	if (!m_wasCrouching)
+			// 	{
+			// 		m_wasCrouching = true;
+			// 		OnCrouchEvent.Invoke(true);
+			// 	}
+
+			// 	// Reduce the speed by the crouchSpeed multiplier
+			// 	move *= m_CrouchSpeed;
+
+			// 	// Disable one of the colliders when crouching
+			// 	if (m_CrouchDisableCollider != null)
+			// 		m_CrouchDisableCollider.enabled = false;
+			// } else
+			// {
+			// 	// Enable the collider when not crouching
+			// 	if (m_CrouchDisableCollider != null)
+			// 		m_CrouchDisableCollider.enabled = true;
+
+			// 	if (m_wasCrouching)
+			// 	{
+			// 		m_wasCrouching = false;
+			// 		OnCrouchEvent.Invoke(false);
+			// 	}
+			// }
+
+			// Move the character by finding the target velocity
+			Vector3 targetVelocity = new Vector2(move * 10f, rb.velocity.y);
+			// And then smoothing it out and applying it to the character
+			rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmoothing);
+
+			// If the input is moving the player right and the player is facing left...
+			if (move > 0 && !facingRight)
+			{
+				// ... flip the player.
+				Flip();
+			}
+			// Otherwise if the input is moving the player left and the player is facing right...
+			else if (move < 0 && facingRight)
+			{
+				// ... flip the player.
+				Flip();
+			}
+		}
+		// If the player should jump...
+		if (grounded && jump)
+		{
+			// Add a vertical force to the player.
+			grounded = false;
+			rb.AddForce(new Vector2(0f, jumpSpeed));
+		}
+	}
+
+
+	private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		facingRight = !facingRight;
+
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
--- GameZone2/Assets/Scripts/PlayerMovement.cs
+++ GameZone2/Assets/Scripts/PlayerMovement.cs
@@ -1,118 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    public float moveSpeed = 5f;
-    public float jumpForce = 25f;
-    Animator animator;
+public class PlayerMovement : MonoBehaviour {
 
-    [SerializeField] Rigidbody2D rb;
-    private GroundCheck groundCheck;
-    private bool isJumping = false;
-    private bool IsRolling = false;
-    private SpriteRenderer spriteRenderer;
+	public CharacterController2D controller;
+	public Animator animator;
 
-    void Start()
-    {
-        groundCheck= GetComponent<GroundCheck>();
-        rb = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
-    }
-    
-    void Update()
-    {
-        #region Movement
-        float moveX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+	public float runSpeed = 40f;
 
-        if (moveX != 0 && !isJumping)
-        {
-            Running();
-        }
-        else if (moveX != 0 && isJumping)
-        {
-            Jumping();
-        }
-        else if(moveX == 0 && !isJumping)
-        {
-            Idle();
-        }
-        
+	float horizontalMove = 0f;
+	bool jump = false;
+	// bool crouch = false;
 
-        if (Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W))
-        {
-            if (!isJumping)
-            {
-                rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-                isJumping = true;
-                Jumping();
-            }
-        }
+	// Update is called once per frame
+	void Update () {
 
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        if(Input.GetKeyDown(KeyCode.LeftShift) && !IsRolling)
-        {
-            
-            //StartRollAnimation();
-            
-        }
-        
-        #endregion
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-        #region FlipChar
-        if (moveX < 0)
-        {
-            spriteRenderer.flipX = true;
-        }
-        else if (moveX > 0)
-        {
-            spriteRenderer.flipX = false;
-        }
-        #endregion
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+			animator.SetBool("IsJumping", true);
+		}
 
-    }
-    //TRIGGER KULLAN!!!
-    private void Idle()
-    {
-        animator.SetBool("isRunning", false);
-        animator.SetBool("isZipzip", false);
-        animator.SetBool("isIdle", true);
-        animator.SetBool("isRolling", false);
-    }
-    private void Running()
-    {
-        animator.SetBool("isRunning", true);
-        animator.SetBool("isZipzip", false);
-        animator.SetBool("isIdle", false);
-        
-    }
-    private void Jumping()
-    {
-        animator.SetBool("isRunning", false);
-        animator.SetBool("isZipzip", true);
-        animator.SetBool("isIdle", false);
-        animator.SetBool("isRolling", false);
-    }
-    //private void Rolling()
-    //{
-    //    animator.SetBool("isRunning", false);
-    //    animator.SetBool("isZipzip", false);
-    //    animator.SetBool("isIdle", false);
-    //    animator.SetBool("isRolling", true);
-    //}
+		// if (Input.GetButtonDown("Crouch"))
+		// {
+		// 	crouch = true;
+		// } else if (Input.GetButtonUp("Crouch"))
+		// {
+		// 	crouch = false;
+		// }
 
-    //void StartRollAnimation()
-    //{
-    //    animator.SetBool("isRolling", true);
-    //    IsRolling = true;
-    //    Invoke("EndRollAnimation", -1.5f);
-    //}
+	}
 
-    //void EndRollAnimation()
-    //{
-    //    animator.SetBool("isRolling", false);
-    //    IsRolling = false;
-    //}
-    
+	public void OnLanding ()
+	{
+		animator.SetBool("IsJumping", false);
+        jump = false;
+	}
+
+	// public void OnCrouching (bool isCrouching)
+	// {
+	// 	animator.SetBool("IsCrouching", isCrouching);
+	// }
+
+	void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+		jump = false;
+	}
 }

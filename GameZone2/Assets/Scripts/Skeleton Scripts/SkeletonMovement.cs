ffa0098b4258a349acf660b8a4ce55badbe9e331
Nisa Dost <i.n.dost38@gmail.com>
2023-07-25 17:42:18 +0300

    attack geliştirme v1

--- GameZone2/Assets/Scripts/Skeleton Scripts/SkeletonMovement.cs
+++ GameZone2/Assets/Scripts/Skeleton Scripts/SkeletonMovement.cs
@@ -6,11 +6,14 @@ public class SkeletonMovement : MonoBehaviour
 {
     public Transform pointLeft;
     public Transform pointRight;
+    public Transform player;
     private Transform currentWaypoint;
-    
     private Animator animator;
+
     public float speed = 1.8f;
     public float idleTime = 2f;
+    private float attackRange = 2f;
+    
     private bool isMovingForward;
     private bool isWaiting = false;
 
@@ -23,7 +26,11 @@ public class SkeletonMovement : MonoBehaviour
 
     private void Update()
     {
-        if (!isWaiting)
+        if (player.position.x > pointLeft.position.x && player.position.x < pointRight.position.x)
+        {
+            ChaseAndAttack();
+        }
+        else if (!isWaiting)
         {
             Vector3 direction = (currentWaypoint.position - transform.position).normalized;
 
@@ -42,19 +49,34 @@ public class SkeletonMovement : MonoBehaviour
             }
         }
     }
+    void ChaseAndAttack()
+    {
+        animator.SetBool("isRunning", true);
+
+        Vector3 direction = (player.position - transform.position).normalized;
+
+        float moveAmount = speed * Time.deltaTime;
+
+        transform.Translate(direction * moveAmount);
+
+        if (direction.x > 0 && transform.localScale.x < 0 || direction.x < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+        float playerDistance = Vector3.Distance(transform.position, player.position);
+        if (playerDistance <= attackRange)
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
    
     IEnumerator IdleAndSwitchWaypoint()
     {
         isWaiting = true;
-
-        animator.SetBool("isRunning", false); // Play the idle animation
-
-        yield return new WaitForSeconds(idleTime); // Wait for the specified idle time
-
-        animator.SetBool("isRunning", true); // Resume the running animation
-
-        SwitchWaypoint(); // Switch the waypoint after waiting
-
+        animator.SetBool("isRunning", false);
+        yield return new WaitForSeconds(idleTime);
+        animator.SetBool("isRunning", true);
+        SwitchWaypoint();
         isWaiting = false;
     }
 
@@ -76,6 +98,12 @@ public class SkeletonMovement : MonoBehaviour
         Vector3 localScale = transform.localScale;
         localScale.x *= -1;
         transform.localScale = localScale;
+    }
+    //animasyon kaydırması denemesi
+    void AnimationSlide(){
+        //Vector3 localPosition = transform.localPosition;
+        //localPosition.x += 0.4f;
+        //transform.localPosition = localPosition;
     }
     private void OnDrawGizmos()
     {
